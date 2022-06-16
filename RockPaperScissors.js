9671ed35a76c3f3d8941eef5d7d48850209f6216
Julian <juleichen@aol.com>
2022-06-16 14:30:40 +0200

    gameplay and buttons

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -41,9 +41,32 @@ while (play) {
 */
 // console.log("Thanks for palying.");
 
-function playRound(playerChoice){
+function playRound(playerDraw){
     const status = document.querySelector('.screen-status');
-    console.log(playerChoice);
+    
+    // compute robot play
+    let roboHand = ['R', 'P', 'S'];
+    let roboDraw = roboHand[Math.floor(Math.random() * roboHand.length)];
+
+    if (playerDraw == 'R' && roboDraw == 'P') {
+        console.log("RPSminator chose PAPER, YOU LOSE!");
+    } else if (playerDraw == 'R' && roboDraw == 'S') {
+        console.log("RPSminator chose SCISSORS, YOU WIN!");
+    } else if (playerDraw == 'P' && roboDraw == 'S') {
+        console.log("RPSminator chose SCISSORS, YOU LOSE!");
+    } else if (playerDraw == 'P' && roboDraw == 'R') {
+        console.log("RPSminator chose ROCK, YOU WIN!");
+    } else if (playerDraw == 'S' && roboDraw == 'P') {
+        console.log("RPSminator chose PAPER, YOU WIN!");
+    } else if (playerDraw == 'S' && roboDraw == 'R') {
+        console.log("RPSminator chose ROCK, YOU LOSE!");
+    } else if (playerDraw == roboDraw) {
+        console.log(`RPSminator chose ${roboDraw}, DRAW!`);
+    } else {
+        console.log("OH NO I CAN'T BELIVE THIS IS HAPPENING!");
+    }
+
+
 }
 
 const container = document.querySelector('#container');
