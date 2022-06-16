af95d6deeb72a84da67d3c1179f3d787033db3a7
Julian <juleichen@aol.com>
2022-06-16 14:51:29 +0200

    working status screen

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -42,28 +42,28 @@ while (play) {
 // console.log("Thanks for palying.");
 
 function playRound(playerDraw){
-    const status = document.querySelector('.screen-status');
+    const status = document.querySelector('#status');
     
     // compute robot play
     let roboHand = ['R', 'P', 'S'];
     let roboDraw = roboHand[Math.floor(Math.random() * roboHand.length)];
 
     if (playerDraw == 'R' && roboDraw == 'P') {
-        console.log("RPSminator chose PAPER, YOU LOSE!");
+        status.textContent = "RPSminator chose PAPER, YOU LOSE!";
     } else if (playerDraw == 'R' && roboDraw == 'S') {
-        console.log("RPSminator chose SCISSORS, YOU WIN!");
+        status.textContent = "RPSminator chose SCISSORS, YOU WIN!";
     } else if (playerDraw == 'P' && roboDraw == 'S') {
-        console.log("RPSminator chose SCISSORS, YOU LOSE!");
+        status.textContent = "RPSminator chose SCISSORS, YOU LOSE!";
     } else if (playerDraw == 'P' && roboDraw == 'R') {
-        console.log("RPSminator chose ROCK, YOU WIN!");
+        status.textContent = "RPSminator chose ROCK, YOU WIN!";
     } else if (playerDraw == 'S' && roboDraw == 'P') {
-        console.log("RPSminator chose PAPER, YOU WIN!");
+        status.textContent = "RPSminator chose PAPER, YOU WIN!";
     } else if (playerDraw == 'S' && roboDraw == 'R') {
-        console.log("RPSminator chose ROCK, YOU LOSE!");
+        status.textContent = "RPSminator chose ROCK, YOU LOSE!";
     } else if (playerDraw == roboDraw) {
-        console.log(`RPSminator chose ${roboDraw}, DRAW!`);
+        status.textContent = `RPSminator chose ${roboDraw}, DRAW!`;
     } else {
-        console.log("OH NO I CAN'T BELIVE THIS IS HAPPENING!");
+        status.textContent = "OH NO I CAN'T BELIVE THIS IS HAPPENING!";
     }
 
 
